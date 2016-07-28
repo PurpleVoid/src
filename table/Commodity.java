eb7978c85f3f5ba29f5a6bc86e1582aba554dcef
PurpleVoid <1198741723@qq.com>
2016-07-28 14:02:43 +0800

    Update Commodity.java

--- table/Commodity.java
+++ table/Commodity.java
@@ -14,6 +14,7 @@ public class Commodity {
 	private String unitName;
 	private Integer providerID;
 	private Calendar inDate;
+	private Integer commodityNumber;
 	
 	public Integer getCommodityID() {
 		return commodityID;
@@ -75,7 +76,12 @@ public class Commodity {
 	public void setUnitName(String unitName) {
 		this.unitName = unitName;
 	}
-	
+	public Integer getCommodityNumber(){
+		return commodityNumber;
+	}
+	public void setCommodityNumber(Integer commodityNumber) {
+		this.commodityNumber = commodityNumber;
+	}
 	
 	
 }
